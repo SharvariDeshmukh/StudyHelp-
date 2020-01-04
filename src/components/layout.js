91a145fb051bb76945a4143eb39f88110a814ba4
SharvariDeshmukh <42609095+SharvariDeshmukh@users.noreply.github.com>
2020-01-04 22:10:51 +0530

    just an edit

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 
 import "./layout.css"
-import "./Bootstrap.min.css"
+import "./bootstrap.min.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
