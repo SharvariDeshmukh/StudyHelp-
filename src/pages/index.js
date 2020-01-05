197d8886b24aeae08d66e4a5f0a60f86a432b509
SharvariDeshmukh <42609095+SharvariDeshmukh@users.noreply.github.com>
2020-01-05 14:54:02 +0530

    hero_ css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,21 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import HeroSection from "../components/HeroSection";
 import Heading from "../components/Heading"
-
+import { graphql } from 'gatsby'
 import Semester from "../components/Semester"
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <Heading title="Study Help"/>
+    <HeroSection
+    img={data.img.childImageSharp.fluid}
+      title="I write Code"
+      subtitle="learncodeonline.in"
+      heroclass="hero-background"
+    
+    ></HeroSection>
+    
    
     <Semester papers={data.papers}/>
     
@@ -17,8 +25,14 @@ const IndexPage = ({data}) => (
 
 
 export const query = graphql`
-{
-  
+{   
+    img: file(relativePath: { eq: "hero.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+        ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+  }
     papers: allContentfulPapers{
       edges{
         node{
